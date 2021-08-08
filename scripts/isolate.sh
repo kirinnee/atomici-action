23406935d3fc84fd3dc690ebea4ee71ff2ae84a0
kirinnee <ernest@eng.atomi.cloud>
2021-08-08 17:26:11 +0800

    feat: sample

--- scripts/isolate.sh
+++ scripts/isolate.sh
@@ -5,7 +5,7 @@ container_id=$(docker run -id -w=/workspace $@ nixos/nix)
 
 cleanup() {
 	echo "Clean up containers removing containers..."
-	docker stop "${container_id}"
+	docker kill "${container_id}"
 	docker rm "${container_id}"
 }
 trap cleanup EXIT
